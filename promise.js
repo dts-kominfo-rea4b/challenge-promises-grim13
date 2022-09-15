a5b0e6c670986c5b5fa61cfaed72d60139e9b0a9
muhammad bunyamin <bunyamin250391@gmail.com>
2022-09-16 00:16:31 +0700

    done challege promise

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async function (param){
+  let ixx = await promiseTheaterIXX()
+  let vcg = await promiseTheaterVGC()
+  let count = 0
+  for(i of ixx) {
+    count = (i.hasil == param) ? (count+1) : count
+  } 
+  for(i of vcg) {
+    count = (i.hasil == param) ? (count+1) : count
+  } 
+  return count
+};
 
 module.exports = {
   promiseOutput,
